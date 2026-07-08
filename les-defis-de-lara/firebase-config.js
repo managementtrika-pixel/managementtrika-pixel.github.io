b726e49104253de535705ac8eca9c68c5d2f3a69
managementtrika-pixel <management.trika@gmail.com>
2026-07-08 21:40:22 +0200

    Configure Firebase web app

--- les-defis-de-lara/firebase-config.js
+++ les-defis-de-lara/firebase-config.js
@@ -1,4 +1,9 @@
-// Configuration Firebase de l'application.
-// Remplace null par l'objet firebaseConfig récupéré dans Firebase Console > Paramètres du projet > Tes applications > Web.
-// Exemple : window.LARA_FIREBASE_CONFIG = { apiKey:'...', authDomain:'...', projectId:'...', storageBucket:'...', messagingSenderId:'...', appId:'...' };
-window.LARA_FIREBASE_CONFIG = null;
+window.LARA_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBnEXmtur43V0hEdEK_-Vbd8OJHaLm5dLc",
+  authDomain: "les-defi-de-lara.firebaseapp.com",
+  projectId: "les-defi-de-lara",
+  storageBucket: "les-defi-de-lara.firebasestorage.app",
+  messagingSenderId: "915695078535",
+  appId: "1:915695078535:web:04384cd12accf7282ebc22",
+  measurementId: "G-M43RR86SX2"
+};
